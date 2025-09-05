d4757282e7d4b9261f9b85cd5046b3ec934cd325
VNagorniy <vnagorniyrnd@gmail.com>
2025-09-05 19:39:35 +0300

    feat: data updating, tagTypes

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -13,27 +13,32 @@ export const playlistsApi = createApi({
 			return headers;
 		}
 	}),
+	tagTypes: ['Playlist'],
 	endpoints: (build) => ({
 		// `query` по умолчанию создает запрос `get` и указание метода необязательно
 		//типизация 1й аргумент - что возвращает запрос, 2й аргумент - тип аргумента
 		fetchPlaylists: build.query<PlaylistsResponse, FetchPlaylistsArgs>({
-			query: () => `playlists`
+			query: () => `playlists`,
+			providesTags: ['Playlist']
 		}),
 		createPlaylist: build.mutation<{ data: PlaylistData }, CreatePlaylistArgs>({
-			query: (body) => ({ method: 'post', url: 'playlists', body })
+			query: (body) => ({ method: 'post', url: 'playlists', body }),
+			invalidatesTags: ['Playlist']
 		}),
 		deletePlaylist: build.mutation<void, string>({
 			query: (playlistId) => ({
 				url: `playlists/${playlistId}`,
 				method: 'delete'
-			})
+			}),
+			invalidatesTags: ['Playlist']
 		}),
 		updatePlaylist: build.mutation<void, { playlistId: string; body: UpdatePlaylistArgs }>({
 			query: ({ playlistId, body }) => ({
 				url: `playlists/${playlistId}`,
 				method: 'put',
 				body
-			})
+			}),
+			invalidatesTags: ['Playlist']
 		})
 	})
 });
