2bf347a87232adc0dcf329d5fc444532756c3dfc
VNagorniy <vnagorniyrnd@gmail.com>
2025-09-05 19:48:16 +0300

    feat: code splitting RTK Query

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -1,19 +1,7 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { baseApi } from '@/app/api/baseApi';
 import type { CreatePlaylistArgs, FetchPlaylistsArgs, PlaylistData, PlaylistsResponse, UpdatePlaylistArgs } from './playlistsApi.types';
 
-export const playlistsApi = createApi({
-	reducerPath: 'playlistsApi',
-	baseQuery: fetchBaseQuery({
-		baseUrl: import.meta.env.VITE_BASE_URL,
-		headers: {
-			'API-KEY': import.meta.env.VITE_API_KEY
-		},
-		prepareHeaders: (headers) => {
-			headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`);
-			return headers;
-		}
-	}),
-	tagTypes: ['Playlist'],
+export const playlistsApi = baseApi.injectEndpoints({
 	endpoints: (build) => ({
 		// `query` по умолчанию создает запрос `get` и указание метода необязательно
 		//типизация 1й аргумент - что возвращает запрос, 2й аргумент - тип аргумента
