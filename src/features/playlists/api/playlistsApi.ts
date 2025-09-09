fcfe9f800ee2cca4456a6967dee5e0c1617335e6
VNagorniy <vnagorniyrnd@gmail.com>
2025-09-09 20:45:54 +0300

    feat: query params, searching, pagination, debounce

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -7,7 +7,7 @@ export const playlistsApi = baseApi.injectEndpoints({
 		// `query` по умолчанию создает запрос `get` и указание метода необязательно
 		//типизация 1й аргумент - что возвращает запрос, 2й аргумент - тип аргумента
 		fetchPlaylists: build.query<PlaylistsResponse, FetchPlaylistsArgs>({
-			query: () => `playlists`,
+			query: (params) => ({ url: `playlists`, params }),
 			providesTags: ['Playlist']
 		}),
 		createPlaylist: build.mutation<{ data: PlaylistData }, CreatePlaylistArgs>({
--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -1,58 +1,40 @@
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useDeletePlaylistMutation, useFetchPlaylistsQuery } from '../../api/playlistsApi';
-import type { PlaylistData, UpdatePlaylistArgs } from '../../api/playlistsApi.types';
+import { Pagination } from '@/common/components/Pagination/Pagination';
+import { useDebounceValue } from '@/common/hooks';
+import { useState, type ChangeEvent } from 'react';
+import { useFetchPlaylistsQuery } from '../../api/playlistsApi';
 import { CreatePlaylistForm } from '../CreatePlaylistForm/CreatePlaylistForm';
-import { EditPlaylistForm } from '../EditPlaylistForm/EditPlaylistForm';
-import { PlaylistItem } from '../PlaylistItem/PlaylistItem';
+import { PlaylistsList } from '../PlaylistsList/PlaylistsList';
 import s from './PlaylistsPage.module.css';
 
 export const PlaylistsPage = () => {
-	const { data } = useFetchPlaylistsQuery();
+	const [search, setSearch] = useState('');
+	const [currentPage, setCurrentPage] = useState(1);
+	const [pageSize, setPageSize] = useState(2);
 
-	const [playlistId, setPlaylistId] = useState<string | null>(null);
-	const { register, handleSubmit, reset } = useForm<UpdatePlaylistArgs>();
+	const debounceSearch = useDebounceValue(search);
+	const { data, isLoading } = useFetchPlaylistsQuery({
+		search: debounceSearch,
+		pageNumber: currentPage,
+		pageSize
+	});
 
-	const [deletePlaylist] = useDeletePlaylistMutation();
-
-	const deletePlaylistHandler = (playlistId: string) => {
-		if (confirm('Are you sure you want to delete the playlist?')) {
-			deletePlaylist(playlistId);
-		}
+	const changePageSizeHandler = (size: number) => {
+		setPageSize(size);
+		setCurrentPage(1);
 	};
 
-	const editPlaylistHandler = (playlist: PlaylistData | null) => {
-		if (playlist) {
-			setPlaylistId(playlist.id);
-			reset({
-				title: playlist.attributes.title,
-				description: playlist.attributes.description,
-				tagIds: playlist.attributes.tags.map((t) => t.id)
-			});
-		} else {
-			setPlaylistId(null);
-		}
+	const searchPlaylistHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearch(e.currentTarget.value);
+		setCurrentPage(1);
 	};
 
 	return (
 		<div className={s.container}>
 			<h1>Playlists page</h1>
 			<CreatePlaylistForm />
-			<div className={s.items}>
-				{data?.data.map((playlist) => {
-					const isEditing = playlistId === playlist.id;
-
-					return (
-						<div className={s.item} key={playlist.id}>
-							{isEditing ? (
-								<EditPlaylistForm playlistId={playlistId} handleSubmit={handleSubmit} register={register} editPlaylist={editPlaylistHandler} setPlaylistId={setPlaylistId} />
-							) : (
-								<PlaylistItem playlist={playlist} deletePlaylist={deletePlaylistHandler} editPlaylist={editPlaylistHandler} />
-							)}
-						</div>
-					);
-				})}
-			</div>
+			<input type="search" placeholder={'Search playlist by title'} onChange={searchPlaylistHandler} />
+			<PlaylistsList playlists={data?.data || []} isPlaylistsLoading={isLoading} />
+			<Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} pagesCount={data?.meta.pagesCount || 1} pageSize={pageSize} changePageSize={changePageSizeHandler} />
 		</div>
 	);
 };
